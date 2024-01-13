fcf601017bc9b9369b6df67605bcf47d0ead654b
ksabiev <ksabiev@gmail.com>
2024-01-13 09:12:14 +0600

    First

---
+++ main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def index(request):
+    context = {
+        'title': 'Home',
+        'content': 'Главная страница'
+    }
+
+    return render(request, 'main/index.html', context)
